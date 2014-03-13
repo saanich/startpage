e673987c0be3b7b0fa3d513a66ef08c3e7d6cedc
saanich <joel.friesen@saanich.ca>
2014-03-13 14:44:28 -0700

    more content

--- js/fancysettings.js
+++ js/fancysettings.js
@@ -1,8 +1,6 @@
  $(document).ready(function() {
  	$("a[rel=fancygroup]").fancybox({
- 		'transitionIn': 'none',
- 		'transitionOut': 'none',
- 		'titlePosition': 'over',
+ 		'titlePosition': 'inside',
  		'titleFormat': function(title, currentArray, currentIndex, currentOpts) {
  			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + ' ' + title + '</span>';
  		}
